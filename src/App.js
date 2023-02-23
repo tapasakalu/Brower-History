b90fcbfb656e983da38b07bd0859c6780e2bbda1
tapasakalu <pintutk9078@gmail.com>
2023-02-23 15:17:04 +0000

    Brower History Solution

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import HistorySearch from './Component/HistorySearch'
+
+import './App.css'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <HistorySearch />
+
+export default App
